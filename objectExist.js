5ffb5d32965c7f2fdd15666bf262e6c180f77cdd
Eric Vera <ev@Erics-MacBook.local>
2018-08-10 21:13:38 -0300

    Fix bug in check of objectExist

--- objectExist.js
+++ objectExist.js
@@ -2,7 +2,7 @@ const checkIf = require('check-if')
 const { UserInputError } = require('apollo-server')
 
 module.exports = (object, attributeName, attributeValue) => {
-  if (checkIf.isNonDeletedObject(object)) {
+  if (checkIf.isNonEmptyObject(object)) {
     throw new UserInputError(`'${attributeName}' '${attributeValue}' exists.`)
   }
 }
